40be453cafc7b4e551d6f39ac282399c6b1f209e
toxic-prashant <yadavprashant1016@gmail.com>
2024-11-07 13:44:19 +0530

    remove unused Vite configuration and related files

--- music-app/src/components/Navbar.tsx
+++ music-app/src/components/Navbar.tsx
@@ -1,11 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import {
-  HoveredLink,
-  Menu,
-  MenuItem,
-  ProductItem,
-} from "@/components/ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
